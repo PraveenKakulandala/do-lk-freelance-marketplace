557f0e88f44dad4f94860268391ec405808f5f33
Praveen Kakulandala <137377079+PraveenKakulandala@users.noreply.github.com>
2025-04-01 22:49:29 +0530

    initialize Firebase setup and functions
    - Initialized Firebase in the project
    - Configured Firestore, Authentication, and Emulators
    - Set up Cloud Functions directory
    - Fixed predeploy issues and dependencies

---
+++ firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB0Yt35QG5kgXKLCAKKtfzk5ASzKLCLFVY",
+  authDomain: "device-streaming-3f4e3d53.firebaseapp.com",
+  projectId: "device-streaming-3f4e3d53",
+  storageBucket: "device-streaming-3f4e3d53.firebasestorage.app",
+  messagingSenderId: "1013061700151",
+  appId: "1:1013061700151:web:b2c0e184fb641066408db2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
